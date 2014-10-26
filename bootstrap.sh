37db998087859007dc8d1090eb6ac44af890123c
ahawkins <adam@hawkins.io>
2014-10-26 21:22:39 +0100

    WIP: save current state

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,9 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Linking xinitc"
 ln -s "$script_dir/xinitrc" ~/.xinitrc
 
+echo "Linking Xresources"
+ln -s "$script_dir/xresources" ~/.Xresources
+
 echo "Linking xmodmap"
 ln -s "$script_dir/xmodmap" ~/.xmodmap
 
